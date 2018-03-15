8e9cda0a975e91b82fdd9289d5c7a52a942ee92b
anlarosa <antonio.larosa@soprasteria.com>
2018-03-15 17:02:40 +0100

    dev

---
+++ SalesTaxes.Fixtures/ConfigurationHelperFixtures.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using SalesTaxes.Entities;
+using SalesTaxes.Services.Taxes.Rules;
+using SalesTaxes.Settings;
+
+namespace SalesTaxes.Fixtures
+{
+    [TestFixture]
+    public class ConfigurationHelperFixtures
+    {
+        private ConfigurationHelper _configurationHelper;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _configurationHelper = new ConfigurationHelper();
+        }
+
+        [Test]
+        public void GetTaxSettings_AppSettingsWithFlatTaxes_MappedFlatTaxes()
+        {
+            //Arrange
+            IConfigurationRoot configurationRoot = Helper.GetConfigurationRoot("appsettingsWithFlatTaxes.json");
+
+            //Act
+            TaxSettings taxSettings = _configurationHelper.GetTaxSettings(configurationRoot);
+
+            //Assert
+            Assert.AreEqual(0, taxSettings.ImportedTaxes.Count);
+            Assert.AreEqual(1, taxSettings.FlatTaxes.Count);
+
+            FlatTax flatTax = taxSettings.FlatTaxes.Single();
+            Assert.AreEqual(0.1m, flatTax.Rate);
+            Assert.AreEqual("Basic Sales Tax", flatTax.Description);
+            Assert.AreEqual(3, flatTax.ExcludedCategories.Count);
+            Assert.AreEqual(CategoryType.Books, flatTax.ExcludedCategories.First());
+            Assert.AreEqual(CategoryType.Foods, flatTax.ExcludedCategories.Skip(1).First());
+            Assert.AreEqual(CategoryType.MedicalProducts, flatTax.ExcludedCategories.Skip(2).First());
+        }
+    }
+}
---
+++ SalesTaxes.Fixtures/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace SalesTaxes.Fixtures
+{
+    public static class Helper
+    {
+        public static IConfigurationRoot GetConfigurationRoot(string jsonFile)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(jsonFile);
+
+            IConfigurationRoot configuration = builder.Build();
+            return configuration;
+        }
+    }
+}
